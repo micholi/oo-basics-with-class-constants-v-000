814aa43814c2b56bd5239e901cd6ef2322487ee0
Michelle Olivieri <michelleo72@gmail.com>
2018-03-23 19:23:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    BRANDS.none? {|temp_brand| temp_brand == @brand}
+    BRANDS.none? {|temp_brand| temp_brand == brand}
     @brand = brand
     BRANDS << brand
   end
