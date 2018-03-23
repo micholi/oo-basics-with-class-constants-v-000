9f16d8df082afbffea07f73a43e54ecdc00a0970
Michelle Olivieri <michelleo72@gmail.com>
2018-03-23 19:47:52 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,20 +4,11 @@ class Shoe
 
   BRANDS = []
 
-
-
   def initialize(brand)
     @brand = brand
     BRANDS.none? {|temp_brand| temp_brand == brand}
     BRANDS << brand
   end
-
-  def brand=(brand)
-    @brand = brand
-    BRANDS.map {|temp_brand| temp_brand != brand}
-    BRANDS << brand
-  end
-
 
   def cobble
     self.condition = "new"
